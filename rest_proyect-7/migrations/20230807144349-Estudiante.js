f2f693d3e30c55ccc706f2435eb0930ef2dc0b32
Cliente CONTROL-PC <CltControl@GGIECLUGWRK176.espol.edu.ec>
2023-08-07 10:49:53 -0500

    'update'

---
+++ rest_proyect-7/migrations/20230807144349-Estudiante.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Estudiante = require('../models').Estudiante;
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await Estudiante.sync()
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('Estudiante');
+  }
+};
---
+++ rest_proyect-7/migrations/20230807144552-Actividad.js
@@ -0,0 +1,24 @@
+'use strict';
+const actividad = require('../models').Actividad;
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await actividad.sync()
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('Actividad');
+  }
+};
