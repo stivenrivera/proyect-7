ac5bad30a7bb208a9f6ef4d051eff14ecc56da58
riverastiven <stjorive@espol.edu.ec>
2023-08-07 07:29:58 -0500

    'update'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import { FlexLayoutModule } from "@angular/flex-layout";
+  
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { ReportComponent } from './pages/report/report.component';
+import { AboutComponent } from './pages/about/about.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ReportComponent,
+    AboutComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+	MatSidenavModule,
+	MatListModule,
+	MatIconModule,
+	MatToolbarModule,
+	MatCardModule,
+	FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
